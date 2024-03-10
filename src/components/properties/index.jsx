9ebcbdfd4b2209c0bfae89966f9dae216f3c607d
John Angelo S. Soriano <sorianojohn008@gmail.com>
2024-03-10 16:21:15 +0800

    add proxy routing for backend

--- src/components/properties/index.jsx
+++ src/components/properties/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { collection, doc, getDocs } from "firebase/firestore";
-import { firestore } from "../../firebase/firebase";
 import CardLayout from "./CardLayout";
 import Card from "./Card";
 import AddPropertyModal from "./AddPropertyModal";
 
 const Properties = () => {
-  const [properties, setProperties] = useState(null);
-  const [refresh, setRefresh] = useState(0);
   const [tableData, setTableData] = useState([]);
   const [addProperty, setAddProperty] = useState(false);
 
@@ -17,13 +13,9 @@ const Properties = () => {
     fetch("api/properties/get-properties")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
-        data.forEach((doc) => {
-          const id = doc.id;
-          setTableData([...tableData, { id, ...doc }]);
-        });
+        setTableData(data);
       });
+    console.log(JSON.stringify(tableData));
   }, []);
 
   const changeAddProperty = () => {
@@ -39,8 +31,9 @@ const Properties = () => {
       {addProperty && <AddPropertyModal cancel={changeAddProperty} />}
       <CardLayout>
         {tableData.map((data) => (
-          <Card key={data.id} data={data} />
+          <Card key={data._id} data={data} />
         ))}
+        <p>{JSON.stringify(tableData)}</p>
       </CardLayout>
     </>
   );
