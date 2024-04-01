ce24330b408c086b53836e0e8b65c3acb0aa03ca
JuanJuuaann <hhojohn@gmail.com>
2024-04-02 04:08:07 +0800

    viettrakres

--- src/components/viewTrackers/index.jsx
+++ src/components/viewTrackers/index.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import PropertyInfoDashboard from "../propertyInfoDashboard/";
+import "../viewTrackers/styles.css"
+import mimage from "../../assets/house-1.png"
+import pimage from "../../assets/house-1.png"
+import bimage from "../../assets/assets-1.png"
+import rimage from "../../assets/town-1.png"
 
 const ViewTrackers = () => {
   return (
-    <div>
-      <div className="trackers">
-        <a href="/trackers/maintenance">Maintenance Tracker</a>
-        <br />
-        <a href="/trackers/penalty">Penalty Tracker</a>
-        <br />
-        <a href="/trackers/bills">Bills Tracker</a>
-        <br />
-        <a href="/trackers/rent">Rent Tracker</a>
+    <div className="tracker-container">
+      <div className="Maintenance-card">
+        <a href="/trackers/maintenance">
+        <img src={mimage} alt="Maintenance"/>
+          <h3>Maintenance Tracker</h3>
+        </a>
       </div>
+      <div className="Penalty-card">
+        <a href="/trackers/penalty">
+        <img src={pimage} alt="Maintenance"/>
+          <h3>Penalty Tracker</h3>
+        </a>
+      </div>
+      <div className="Bills-card">
+        <a href="/trackers/bills">
+        <img src={bimage} alt="Maintenance"/>
+          <h3>Bills Tracker</h3>
+        </a>
+      </div>
+      <div className="Rent-card">
+        <a href="/trackers/rent">
+        <img src={rimage} alt="Maintenance"/>
+          <h3>Rent Tracker</h3>
+        </a>
+      </div>
+      {/* Include other tracker cards as needed */}
       <div className="info">
         <PropertyInfoDashboard />
       </div>
